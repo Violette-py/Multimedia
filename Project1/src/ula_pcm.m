2b6d80454f1799dcc326e14b7b758de7237844d1
violette_huipu <21307130470@m.fudan.edu.cn>
2023-10-19 12:51:35 +0800

    finish project 1

--- Project1/src/ula_pcm.m
+++ Project1/src/ula_pcm.m
@@ -8,13 +8,13 @@ function [a_quan]=ula_pcm(a,n,u)
 
 % todo: 
 
-% 利用μ律函数对输入量化器的信号进行压缩处理
-compression_signal = ulaw(a,u);
+% 利用μ律函数对输入信号进行压缩
+compressed_signal = ulaw(a, u);
 
 % 对压缩处理后的信号进行均匀量化
-quantized_signal = u_pcm(compression_signal,n);
+quantized_signal = u_pcm(compressed_signal, n);
 
 % 利用μ律函数的反函数对均匀量化后的信号进行扩张处理
-a_quan = inv_ulaw(quantized_signal,u);
+a_quan = inv_ulaw(quantized_signal, u);
 
 end
--- Project1/src/ulaw.m
+++ Project1/src/ulaw.m
@@ -6,6 +6,8 @@ function [z]=ulaw(y,u)
 % todo: 
 
 % μ律函数（压缩器）
-z = (sign(y) .* log(1 + u * abs((y / max(abs(y)))))) ./ log(1 + u);
+% 根据μ律函数的定义，其输入变量的范围通常在[-1,1]之间，所以需要对y进行归一化处理
+y_max = max(abs(y));
+z = (sign(y) .* log(1 + u * abs((y / y_max)))) ./ log(1 + u);
 
 end
